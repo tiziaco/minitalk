efd18db41c783dfc54ecaf483e86acce1beab4a2
Tiziano Iacovelli <tiacovel@c4b5c1.42berlin.de>
2023-12-13 17:03:50 +0100

    main

--- client.c
+++ client.c
@@ -1,50 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/12 16:11:03 by tiacovel          #+#    #+#             */
-/*   Updated: 2023/12/13 16:48:39 by tiacovel         ###   ########.fr       */
+/*   Created: 2023/12/13 14:55:41 by tiacovel          #+#    #+#             */
+/*   Updated: 2023/12/13 16:47:17 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <sys/wait.h>
 
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
 
-volatile sig_atomic_t signal_received = 0;
-
-void signal_handler(int signal)
+int main(int argc, char **argv)
 {
-	if (signal == SIGUSR1)
-	{
-		printf("Signal 1 recived/n");
-		signal_received = 1;
-	}
-	else if (signal == SIGUSR2)
-	{
-		printf("Signal 2 recived/n");
-		signal_received = 1;
-	}
-}
+	pid_t server_pid;
+	int result;
 
-int main(void)
-{
-	pid_t	pid;
-
-	pid = getpid();
-	printf("Server PID: %d\n", pid);
-	signal(SIGUSR1, signal_handler);
-	//signal(SIGUSR2, signal_handler);
-	printf("Server: Waiting for signals...\n");
-	while (1) 
-	{
-		pause();
+	server_pid = 38693;
+	printf("Client: Sending SIGUSR1 to server.\n");
+	result = kill(server_pid, SIGUSR1);
+	if (result == 0) {
+		printf("Client: Signal sent successfully.\n");
+	} else {
+		perror("Client: Error sending signal");
 	}
+	sleep(1);
 	return (0);
 }
